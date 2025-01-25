deade5153e892a8f3b1b41fe19812c1afa32d9e3
chinmaykd21 <chinmaykd21@gmail.com>
2025-01-25 12:38:30 -0500

    health check endpoint

--- app/main.py
+++ app/main.py
@@ -4,4 +4,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to Real-Time Notes App!"}
+    return { "message": "Welcome to Real-Time Notes App!" }
+
+@app.get("/health")
+def health_check():
+    return { "status": "ok" }
